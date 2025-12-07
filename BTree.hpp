05e923aec03f457717a4ff7aef2357aef9fc7532
Zohaasad <bscs24135@itu.edu.pk>
2025-12-07 08:54:24 +0500

    Create BTree.hpp

---
+++ BTree.hpp
@@ -0,0 +1,48 @@
+
+#ifndef BTREE_HPP
+#define BTREE_HPP
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include "FileEntry.hpp"  // Use the main FileEntry definition
+
+class BTreeNode {
+public:
+    bool isLeaf;
+    int t;
+    int n; // number of keys
+    FileEntry* keys;
+    long* childrenOffsets; // offsets of child nodes
+    long offset;           // offset of this node in file
+
+    BTreeNode(int _t, bool _isLeaf);
+    ~BTreeNode();
+
+    void traverse(std::fstream &file);
+    FileEntry* search(int id, std::fstream &file);
+    void insertNonFull(const FileEntry &entry, std::fstream &file);
+    void splitChild(int i, BTreeNode* y, std::fstream &file);
+    void writeNode(std::fstream &file);
+    void readNode(std::fstream &file, long pos);
+};
+
+class BTree {
+private:
+    BTreeNode* root;
+    int t;
+    std::string filename;
+    std::fstream file;
+
+public:
+    BTree(int _t, const std::string &_filename);
+    ~BTree();
+
+    void insert(const FileEntry &entry);
+    FileEntry* search(int id);
+    void traverse();
+};
+
+#endif
