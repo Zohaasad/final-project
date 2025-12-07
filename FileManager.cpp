2c647a7e27d2dd4b3a3466fd7997cf6dbf6b92be
Zohaasad <bscs24135@itu.edu.pk>
2025-12-07 20:46:42 +0500

    Update FileManager.cpp

--- FileManager.cpp
+++ FileManager.cpp
@@ -2,3 +2,6 @@
 #include "FileManagerDisk.hpp"
 #include <iostream>
 #include <ctime>
+FileManager::FileManager(int initialSize)
+    : files(initialSize), binFiles(initialSize), nextFileId(1), disk(nullptr), currentUserId(-1) {}
+
