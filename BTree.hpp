d7d9c8fdf01f36ed1ca1db653051e263c8f94d87
Zohaasad <bscs24135@itu.edu.pk>
2025-12-09 12:13:59 +0500

    Update BTree.hpp

--- BTree.hpp
+++ BTree.hpp
@@ -1,47 +1,57 @@
 
-#ifndef BTREE_HPP
-#define BTREE_HPP
-
-#include <iostream>
-#include <vector>
+#pragma once
 #include <fstream>
-#include <string>
-#include <cstring>
-#include "FileEntry.hpp"  
+#include <vector>
+#include <iostream>
+
+struct FileIndexEntry {
+    int fileId;
+    int firstBlock;
+    bool inUse;
+
+    FileIndexEntry(int fId = 0, int fBlock = -1) : fileId(fId), firstBlock(fBlock), inUse(true) {}
+};
 
 class BTreeNode {
 public:
+    int t; // minimum degree
     bool isLeaf;
-    int t;
-    int n; 
-    FileEntry* keys;
-    long* childrenOffsets; 
-    long offset;          
+    int n; // number of keys
+    FileIndexEntry* keys;
+    long* childrenOffsets;
+    long offset; // position in file
+
     BTreeNode(int _t, bool _isLeaf);
     ~BTreeNode();
 
-    void traverse(std::fstream &file);
-    FileEntry* search(int id, std::fstream &file);
-    void insertNonFull(const FileEntry &entry, std::fstream &file);
-    void splitChild(int i, BTreeNode* y, std::fstream &file);
     void writeNode(std::fstream &file);
     void readNode(std::fstream &file, long pos);
+
+    FileIndexEntry* search(int fileId, std::fstream &file);
+    void traverse(std::fstream &file);
+    void splitChild(int i, BTreeNode* y, std::fstream &file);
+    void insertNonFull(const FileIndexEntry &entry, std::fstream &file);
+    bool removeKey(int fileId, std::fstream &file);
+    void collectFileIds(std::fstream &file, std::vector<int>& ids);
 };
 
 class BTree {
-private:
-    BTreeNode* root;
+public:
     int t;
     std::string filename;
+    BTreeNode* root;
     std::fstream file;
 
-public:
     BTree(int _t, const std::string &_filename);
     ~BTree();
 
-    void insert(const FileEntry &entry);
-    FileEntry* search(int id);
+    void insert(int fileId, int firstBlock);
+    FileIndexEntry* search(int fileId);
+    bool remove(int fileId);
+    std::vector<int> getAllFileIds();
     void traverse();
+private:
+    long rootOffset; // stored in file header
+    void readRoot();
+    void writeRoot();
 };
-
-#endif
