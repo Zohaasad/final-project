a15a7caa2eb34ed7653f41ae238dcdbc663c8b7e
Zohaasad <bscs24135@itu.edu.pk>
2025-12-31 23:36:22 +0500

    Refactor FileManager class and update methods

--- FileManager.hpp
+++ FileManager.hpp
@@ -1,51 +1,54 @@
+
 #ifndef FILEMANAGER_HPP
 #define FILEMANAGER_HPP
 
 #include <string>
 #include <vector>
-#include "HashMap.hpp"
+#include <ctime>
+#include <iostream>
 #include "FileEntry.hpp"
+#include "FileManagerDisk.hpp"
+#include "MinHeap.hpp"
+#include "HashMap.hpp"
 
 class FileManagerDisk;
 
 class FileManager {
 private:
-   private:
-    HashMap<FileEntry> files;       
-    HashMap<FileEntry> binFiles;     
-    int nextFileId;
-    FileManagerDisk* disk;
-    int currentUserId;   
+    HashMap<FileEntry> fileMap;    
+    int currentUserId;
+    FileManagerDisk* diskManager;
+    FileEntryHeap expiryHeap;        
 
 public:
-   
-    FileManager(int initialSize);
+    FileManager(int userId = -1);
+    ~FileManager() = default;
+
     void setCurrentUser(int userId);
-    
-   
-    int getCurrentUser() const;
-    void setDiskManager(FileManagerDisk* diskMgr);
+    void setDiskManager(FileManagerDisk* dm);
+
+    bool loadUserFiles(int userId);   
+    void unloadUserFiles(int userId);  
+
     bool createFile(const std::string& name, const std::string& content, long expireSeconds);
-    bool writeFile(const std::string& fileName, const std::string& content);
-    bool editFile(const std::string& fileName, const std::string& content);
-    
-   
-    bool readFile(const std::string& fileName, std::string& outContent);
-    
-    
-    bool truncateFile(const std::string& fileName);
-    bool moveToBin(const std::string& fileName);
-    bool moveToBinById(int fileId); 
-    bool retrieveFromBin(const std::string& fileName);
-    bool changeExpiry(const std::string& fileName, long newExpireSeconds);
-    FileEntry* searchFile(const std::string& fileName);
+    bool writeFile(const std::string& name, const std::string& content);
+    bool readFile(const std::string& name, std::string& content);
+    bool truncateFile(const std::string& name);
+    bool moveToBin(const std::string& name);
+    bool retrieveFromBin(const std::string& name);
+    bool changeExpiry(const std::string& name, long newExpireSeconds);
+    bool removeFileCompletely(const std::string& name);
+
+    FileEntry* searchFile(const std::string& name);
     FileEntry* searchFileById(int fileId);
-    void listFiles() const;
+    void updateExpiryStatus();
     std::vector<FileEntry> getActiveFiles() const;
     std::vector<FileEntry> getBinFiles() const;
-    bool removeFileCompletely(const std::string& fileName);
+    bool restoreFile(int fileId, const std::string& name, const std::string& content, 
+                     long expireSeconds, int ownerId, bool wasInBin, 
+                     time_t originalCreateTime = 0, time_t originalExpireTime = 0);
 
-    void loadFileEntry(const FileEntry& f);
+    void displayAllFiles() const;
 };
 
-#endif 
+#endif
